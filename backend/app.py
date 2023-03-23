4045e447083f24427777b4613b865a080212436e
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-03-23 09:12:41 -0300

    fixed imports

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
 import os
-from flask import Flask
-from dotenv import load_dotenv
-
-load_dotenv()
-app = Flask(__name__)
+from main import create_app
+app = create_app
+app.app_context().push()
 
 if __name__=="__main__":
     app.run(debug=True, port=os.getenv("PORT"))
--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+
+
 def create_app():
     load_dotenv()
     app = Flask(__name__)
     return app
-
-if __name__=="__main__":
-    app = create_app()
-    app.run(debug=True, port=os.getenv("PORT"))
