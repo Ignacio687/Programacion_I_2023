7819428faf93eaf78e405a500842fe1dd7979800
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-04-24 17:16:50 -0300

    not working u know

--- backend/main/models/planificacion.py
+++ backend/main/models/planificacion.py
@@ -5,27 +5,32 @@ class Planificacion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     profesor_DNI = db.Column(db.Integer(8), nullable=False)
     alumno_DNI = db.Column(db.Boolean(8), nullable=False)
-    estado = db.Column(db.Boolean(), default=False, )
+    estado = db.Column(
+        db.Boolean(),
+        default=False,
+    )
     creation_date = db.Column(db.Date, nullable=False)
+
     def __repr__(self):
-        return f'Planificacion {self.id}'
+        return f"<Planificacion {self.id}>"
+
     # Convertir objeto en JSON
 
     def to_json(self):
         plan_json = {
-            'id': id,
-            'profesor_DNI': self.profesor_DNI,
-            'alumno_DNI': self.alumno_DNI,
-            'estado': self.estado,
-            'creation_date': self.creation_date,
+            "id": id,
+            "profesor_DNI": self.profesor_DNI,
+            "alumno_DNI": self.alumno_DNI,
+            "estado": self.estado,
+            "creation_date": self.creation_date,
         }
         return plan_json
 
     def to_json_short(self):
         plan_json = {
-            'alumno_DNI': self.alumno_DNI,
-            'estado': self.estado,
-            'creation_date': self.creation_date,
+            "alumno_DNI": self.alumno_DNI,
+            "estado": self.estado,
+            "creation_date": self.creation_date,
         }
         return plan_json
 
@@ -33,8 +38,8 @@ class Planificacion(db.Model):
     # Convertir JSON a objeto
     def from_json(plan_json):
         return Planificacion(
-            profesor_DNI=plan_json['profesor_DNI'],
-            alumno_DNI=plan_json['alumno_DNI'],
-            estado=plan_json['estado'],
-            creation_date=plan_json['creation_date']
+            profesor_DNI=plan_json["profesor_DNI"],
+            alumno_DNI=plan_json["alumno_DNI"],
+            estado=plan_json["estado"],
+            creation_date=plan_json["creation_date"],
         )
--- backend/main/resources/planificacion.py
+++ backend/main/resources/planificacion.py
@@ -1,44 +1,30 @@
 from flask_restful import Resource
-from flask import request
-from app.models import Planificacion
+from flask import request, jsonify
+from main.models import PlanificacionModel
 
 # Datos de prueba en JSON
-PLANIFICACIONES = {
-    1: {'Nombre': 'planificacion1', 'ProfDNI': 77854625,
-        'AlumnoDNI': 49137856, 'Estado': 'activa'},
-    2: {'Nombre': 'planificacion2', 'ProfDNI': 84981773,
-        'AlumnoDNI': 12548952, 'Estado': 'inactiva'},
-    3: {'Nombre': 'planificacion3', 'ProfDNI': 84981773,
-        'AlumnoDNI': 12548952, 'Estado': 'activa'},
-}
+# PLANIFICACIONES = {
+#     1: {'Nombre': 'planificacion1', 'ProfDNI': 77854625,
+#         'AlumnoDNI': 49137856, 'Estado': 'activa'},
+#     2: {'Nombre': 'planificacion2', 'ProfDNI': 84981773,
+#         'AlumnoDNI': 12548952, 'Estado': 'inactiva'},
+#     3: {'Nombre': 'planificacion3', 'ProfDNI': 84981773,
+#         'AlumnoDNI': 12548952, 'Estado': 'activa'},
+# }
 
 
-class PlanificacionAlumnoResource(Resource):
-    # ... otros métodos aquí ...
-
-    def get(self):
+class PlanificacionAlumno(Resource):
+    def get(self, id):
         # Obtener la planificación por defecto
-        planificacion = Planificacion.query.first()
-        # Si no hay ninguna planificación en la base de datos, devolver un mensaje de error
-        if planificacion is None:
-            return {'error': 'No se encontró ninguna planificación'}, 404
-        # Devolver la planificación como JSON
+        planificacion = db.session.query(PlanificacionModel).filter_by(alumno_DNI=id).all().get_or_404(id)
         return planificacion.to_json()
 
-
 class PlanificacionesProfesores(Resource):
-    def get(self, dni=None):
-        if dni != None:
-            planificaciones = {}
-            for id in range(1, len(PLANIFICACIONES)+1):
-                if int(dni) == PLANIFICACIONES[id]['ProfDNI']:
-                    planificaciones[id] = PLANIFICACIONES[id]
-            if len(planificaciones) != 0:
-                return planificaciones
-            return '', 404
-        else:
-            return PLANIFICACIONES
-
+    def get(self, dni):
+        planificacion = db.session.query(PlanificacionModel).filter(profesor_DNI=id).all().get_or_404(id)
+        return planificacion.to_json()
+        
+        
     def post(self):
         planificacion = request.get_json()
         id = int(max(PLANIFICACIONES.keys()))+1
@@ -48,20 +34,19 @@ class PlanificacionesProfesores(Resource):
 
 class PlanificacionProfesor(Resource):
     def get(self, id):
-        if int(id) in PLANIFICACIONES:
-            return PLANIFICACIONES[int(id)]
-        return '', 404
+        planificacion = db.session.query(PlanificacionModel).filter_by(profesor_DNI=id).all().get_or_404(id)
+        return planificacion.to_json()
 
-    def put(self, id):
-        if int(id) in PLANIFICACIONES:
-            planificacion = PLANIFICACIONES[int(id)]
-            data = request.get_json()
-            planificacion.update(data)
-            return '', 201
-        return '', 404
+    # def put(self, id):
+    #     if int(id) in PLANIFICACIONES:
+    #         planificacion = PLANIFICACIONES[int(id)]
+    #         data = request.get_json()
+    #         planificacion.update(data)
+    #         return '', 201
+    #     return '', 404
 
-    def delete(self, id):
-        if int(id) in PLANIFICACIONES:
-            del PLANIFICACIONES[int(id)]
-            return '', 204
-        return '', 404
+    # def delete(self, id):
+    #     if int(id) in PLANIFICACIONES:
+    #         del PLANIFICACIONES[int(id)]
+    #         return '', 204
+    #     return '', 404
